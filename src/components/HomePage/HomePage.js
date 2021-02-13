739fdc0b3b03c8d93e0e29347293ac132b9fadd8
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-02-13 13:17:05 -0500

    updated background

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -7,28 +7,34 @@ class HomePage extends Component {
     render() {
         return(
             <div className="homePageDiv">
-                <Header/>
-
-                <div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div id="content">
-                                    <h1>Classroom Covid</h1>
-                                    <h3>The university database for Covid-19 protocols</h3>
-                                    <hr></hr>
-                                    <Link to="/schools">
-                                        <button className="btn btn-default btn-lg" id = "homepageButton"><i aria-hidden="true"></i>
-                                            View Schools
-                                        </button>
-                                    </Link>
+                <div id="landing-header">
+                    <Header/>
+                    <div>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div id="content">
+                                        <h1>Classroom Covid</h1>
+                                        <h3>The university database for Covid-19 protocols</h3>
+                                        <hr></hr>
+                                        <Link to="/schools">
+                                            <button className="btn btn-default btn-lg" id = "homepageButton"><i aria-hidden="true"></i>
+                                                View Schools
+                                            </button>
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
+                <ul className="slideshow">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         );
     }
